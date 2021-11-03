bcf2867e09be6c56acaa80302983b18ecb03484c
Роман Михайловский <mihailovsky.rv@yandex.ru>
2021-11-03 15:40:20 +0400

    branch editor 3

--- InvCtrl/Program.cs
+++ InvCtrl/Program.cs
@@ -15,6 +15,7 @@ namespace InvCtrl
         static void Main()
         {
             // // branch edition
+            // branch editor 3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
